bda6be444c6a057e6d225d59beef83fcb7a22f8a
Rob Pineault <pineaultr@wit.edu>
2022-08-12 14:02:55 -0400

    response

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,9 +1,11 @@
 import { Button as BootButton } from "react-bootstrap"; // error 'cannot find'
 // i like what u did to call it a bootbutton, to differentiate it
 
-interface ButtonProps { // why not just exclude this and say Button = ({ text }: string) => etc
+interface ButtonProps {
+  // why not just exclude this and say Button = ({ text }: string) => etc
   text: string;
 }
+//
 const Button = ({ text }: ButtonProps) => {
   // link to bootstrap docs
   // https://react-bootstrap.github.io/components/alerts
@@ -12,3 +14,25 @@ const Button = ({ text }: ButtonProps) => {
 };
 
 export default Button;
+
+/*
+A: npm install should fix the 'cannot find'
+running ^ is necessary every time you switch between branches that have different dependencies defined in package.json
+
+B: So a very common syntax for accessing data from an object is deconstructing it (theres probably a specific name for this syntax)...
+
+this component without deconstructing
+const Button = (props: ButtonProps) => { 
+
+  return <BootButton variant="primary">{props.text}</BootButton>;
+};
+
+notice how ButtonProps is describing the type of an object so string wouldn't be correct. you could do this inline like I did in layout or make an interface like i did here
+
+example to show another common deconstruction pattern with nested data
+const Button = ({text, data}: ButtonProps) => { 
+  const {some_primitive1, some_primitive2} = data;
+  return <BootButton variant="primary" attribute={some_primitive1}>{text}</BootButton>;
+};
+
+*/
--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,11 +1,13 @@
 const Layout = ({
   children,
   title,
-}: { // is this part, in brackets, an object?
+}: {
+  // is this part, in brackets, an object?
   children: React.ReactNode;
   title: string;
 }) => {
-  return ( // and is this part in parens an element?
+  return (
+    // and is this part in parens an element?
     <>
       <h1>{title}</h1>
       <main>{children}</main>;
@@ -13,3 +15,30 @@ const Layout = ({
   );
 };
 export default Layout;
+
+/**
+no its defining the type being passed to this function. check button response
+
+not sure i understand the question
+
+layout is a wrapper component; its main function is to be a container for other components
+
+layout use in Home component
+<Layout title="Home">
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <Button text="I do nothing" />
+  </header>
+</Layout>
+
+everything inside Layout is passed as the designated prop name children to layout
+
+equivalent code:
+<Layout title="Home" children={  
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <Button text="I do nothing" />
+  </header>
+} />
+
+ */
