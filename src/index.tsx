1d634f5edae4c79847c6f4e7b9578e81eb0be071
Walker <walkerh4@wit.edu>
2022-09-25 14:02:26 -0400

    Comments 1

--- src/index.tsx
+++ src/index.tsx
@@ -20,3 +20,18 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// need an explanation for line 9.
+// why is the url path just '/'???? How does that make sense
+// everything else is understandable and really cool.
+
+// also this just occurred to me. When I hover over a function or object
+// I get a nice tooltip about what it does or is. How do I have my
+// code do that too? Is it just with header comments? I'm gonna check
+// the node.js source to see...
+// ok yeah so it's just simple header comments and vscode does the
+// heavy lifting.
+
+// does the constructor get called when an element is initialized?
+// example is how App() in App.tsx must get called when <App/> gets called
+// in index.tsx
