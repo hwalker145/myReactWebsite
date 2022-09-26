c21c692299790c402039434bc00831ffc0b8c886
Walker <walkerh4@wit.edu>
2022-09-26 06:56:20 -0400

    Made a comment

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import Button from "../Components/Button";
 import Layout from "../Components/Layout";
+import Background from "../Components/Background"
 import logo from "../logo.svg";
 
 const Home = () => {
@@ -13,3 +14,6 @@ const Home = () => {
   );
 };
 export default Home;
+
+// is everything on lines 9-12 being passed into 
+// the Layout constructor as children?
