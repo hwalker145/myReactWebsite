bfd6a4177ae213a9c9010d9d2232bd5e9f177ec8
Walker <walkerh4@wit.edu>
2022-12-31 07:39:34 -0500

    Rearranging the hierarchy

--- src/Components/Layout.tsx
+++ src/Components/Layout.tsx
@@ -1,19 +1,10 @@
 const Layout = ({
-  children,
-  title,
+  children
 }: {
   children: React.ReactNode;
-  title: string;
 }) => {
   return (
     <main>{children}</main>
   );
 };
 export default Layout;
-
-
-// HENRY 2pm
-// When <Layout title='foo'><blahblah /></Layout> is executed,
-// does this constructor automatically see <blahblah> as children?
-// or do you need to go 
-// <Layout title='foo' children = 'bar'><blahblah></Layout>
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,10 +4,9 @@ import Background from "../Components/Background";
 
 const Home = () => {
   return (
-    <Background/>
+    <Layout>
+      <Background/>
+    </Layout>
   );
 };
 export default Home;
-
-// is everything on lines 9-12 being passed into 
-// the Layout constructor as children? 
